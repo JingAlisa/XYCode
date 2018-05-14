a3213505be1d0f9815f6bc9320877e2f02e3dfc5
JingAlisa <1195327084@qq.com>
2018-05-14 11:04:49 +0800

    完成弹出层

--- components/teamItem/teamItem.js
+++ components/teamItem/teamItem.js
@@ -16,13 +16,23 @@ Component({
    * 组件的初始数据
    */
   data: {
-    icon60:'../../public/img/avatar/16.png'
+    icon60:'../../public/img/avatar/16.png',
+    showCard:false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    showCard:function(e){
+      this.setData({
+        showCard:true
+      })
+    },
+    hideCard:function(e){
+      this.setData({
+        showCard: false
+      })
+    }
   }
 })
